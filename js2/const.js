4c46307fcc0f4a00279d674e88a0112569a662c4
Qaisar Abbas <144895046+qaisarabbas0011@users.noreply.github.com>
2024-09-13 07:25:45 +0000

    container config done

---
+++ js2/const.js
@@ -0,0 +1,14 @@
+const accountId=2334445556
+let accountEmail="abc@gmail.com"
+var accountPassword="12345"
+accountCity="Gilgit"
+/*
+don not use var
+Cuz it can't give me the asurity of block scope 
+and functional scope
+
+*/
+
+console.log(accountId)
+//other techinaque to console
+console.table([accountEmail,accountPassword,accountCity])
---
+++ js2/datatpe.js
@@ -0,0 +1,16 @@
+"use strict";//treat all JS code as newer version
+
+//alert("hello")//we are using node js not browser
+
+
+//premative data type or predefine
+let name1="qaisar"//string(array of character)
+let age=123//number(bigint,numbers)
+let isLoggedIn=false//boolen(true,false)
+//null => standalone vale represent the nullvalue or empty
+//undefine =>not assigning the value of variable
+//symbole =>using for uniqueness
+
+console.log(typeof age)
+console.log(isLoggedIn)
+console.log(name1)
