590ffc2d812f3fd447afd73ad715de31ac955e44
Qaisar Abbas <144895046+qaisarabbas0011@users.noreply.github.com>
2024-09-14 11:05:00 +0000

    container config done

---
+++ js4/nums_and_maths.js
@@ -0,0 +1,58 @@
+// const num=123
+
+// console.log(num);
+
+// const num1=new Number(100)// it tightly spacify the type of veriable
+// console.log(num1)
+
+
+// console.log("giving a conversion of number in str and provide len");
+// console.log(num1.toString().length);
+
+
+// console.log("giving fixed values")
+// console.log(num.toFixed(2));
+
+// const pointval=122.0346
+
+// console.log("presion vale with exponent:")
+// console.log(pointval.toPrecision(2));
+
+// const hundreds=10000000000
+
+// console.log("readable formates");
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// //************************MATHS***********************
+
+console.log(Math);
+console.log("find absloute value:");
+
+console.log(Math.abs(-4));
+console.log("round off with .5 technique:");
+
+console.log(Math.round(4.6));
+console.log("point value reduction and print number before point;");
+
+console.log(Math.ceil(4.9))
+console.log("point value reduction and print next number:");
+console.log(Math.floor(4.3));
+console.log("find min number value in given data set;");
+console.log(Math.min(1,5,2,6,5));
+console.log("find max number in given data set:");
+console.log(Math.max(9,3,4,5,9));
+console.log((Math.random()*10)+1);
+
+const min=12
+
+const max=24
+console.log(Math.floor(Math.random()*(max-min))+min);
+
+
+
+
+
+
+
+
+
