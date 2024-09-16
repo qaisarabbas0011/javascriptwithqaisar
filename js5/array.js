c0444e99d0b9231e1fd17fc38a8ff2fc55363f20
Qaisar Abbas <144895046+qaisarabbas0011@users.noreply.github.com>
2024-09-16 07:18:28 +0000

    container config done

---
+++ js5/array.js
@@ -0,0 +1,36 @@
+const myArr=[1,2,3,4,5,6,]
+
+console.log(myArr);
+
+//method in array
+
+//push used to add an element
+myArr.push(9)
+console.log(myArr);
+
+//pop used to pull or delete an element from end of array
+myArr.pop()
+
+const newArr=myArr.join();// using to covert array in string
+console.log(myArr);
+
+console.log(newArr);
+
+console.log("A",myArr);
+
+const mya1=myArr.slice(1,4)
+
+console.log(mya1);
+
+console.log("B",myArr)
+
+const may2=myArr.splice(1,4)
+
+console.log("C",myArr);
+console.log(may2);
+
+
+
+
+
+
