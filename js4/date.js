b214099b1581f6f22d72d87c2f1251241b1e83b9
Qaisar Abbas <144895046+qaisarabbas0011@users.noreply.github.com>
2024-09-16 06:50:51 +0000

    container config done

---
+++ js4/date.js
@@ -0,0 +1,30 @@
+//****************************DATE**********************
+let myDate=new Date();
+console.log(myDate);
+
+//in form of string
+console.log("Date in String :");
+
+console.log(myDate.toString());
+
+console.log("local string :");
+
+console.log(myDate.toLocaleString());
+
+let myCreatedDate= new Date(2024,0,23)
+
+console.log(myCreatedDate.toDateString());
+
+console.log(myCreatedDate.toLocaleString());
+
+//*****************Timestamp******************
+
+let myTimeStamp= Date.now();
+
+console.log(myTimeStamp);
+
+
+let newDate=new Date()
+
+console.log(newDate.getMonth());
+
